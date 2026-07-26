b12e8a9e0d98c4c4d3cdeff2a76a140f52b1885c
Adrianna Jeba <anderson77845568@gmail.com>
2026-07-26 09:59:53 +0300

    Update index.php

--- pp-content/pp-install/index.php
+++ pp-content/pp-install/index.php
@@ -158,7 +158,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="author" content="QubePlug Bangladesh">
+    <meta name="author" content="Mopara Pair Ayat">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Installer - SiratPay</title>
     <link rel="shortcut icon" href="<?= $piprapay_favicon ?? '' ?>">
